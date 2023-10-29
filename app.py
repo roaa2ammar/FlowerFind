0ceccafdb1896bb8b5993aa4bc47b0e61748b483
roaa2ammar <roaa2ammar@gmail.com>
2023-10-29 08:52:49 +0000

    Drag and Drop updated & image showing on website

--- app.py
+++ app.py
@@ -11,17 +11,17 @@ model = pickle.load(open('flowers/model.pkl','rb'))
 def home(): 
     return render_template('home.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
+# @app.route('/predict',methods=['POST'])
+# def predict():
 
-    uploaded_file = request.files['image']
-    if uploaded_file.filename != '':
-        #save to temporary file path
-        image_path = '/temp/image.jpg'
-        uploaded_file.save(image_path)
+#     uploaded_file = request.files['image']
+#     if uploaded_file.filename != '':
+#         #save to temporary file path
+#         image_path = '/temp/image.jpg'
+#         uploaded_file.save(image_path)
 
 
-    return ""
+#     return ""
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- static/home.js
+++ static/home.js
@@ -4,39 +4,126 @@ document.addEventListener("DOMContentLoaded", function(){
     heading.style.transform= "scale(1)";
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const dropContainer = document.getElementById("dropContainer");
-    const dropZone = document.getElementById("dropZone");
-    const fileInput = document.getElementById("fileInput");
+//selecting all required elements
+const dropArea = document.querySelector(".drag-area"),
+dragText = dropArea.querySelector("header"),
+button = dropArea.querySelector("button"),
+input = dropArea.querySelector("input");
+let file; //this is a global variable and we'll use it inside multiple functions
+var fileobj;
 
-    dropContainer.addEventListener("dragover", function(e) {
-        e.preventDefault();
-        dropZone.classList.add('highlight');
-    });
 
-    dropContainer.addEventListener("dragleave", function() {
-        dropZone.classList.remove('highlight');
-    });
+button.onclick = ()=>{
+  input.click(); //if user click on the button then the input also clicked
+  file_browse();
+}
 
-    dropContainer.addEventListener("drop", function(e) {
-        e.preventDefault();
-        dropZone.classList.remove('highlight');
-
-        const files = e.dataTransfer.files;
-        handleFiles(files);
-    });
-
-    fileInput.addEventListener("change", function() {
-        const files = fileInput.files;
-        handleFiles(files);
-    });
-
-    function handleFiles(files) {
-        // Handle the dropped or selected files here
-        console.log(files);
-        // You can add further processing for the files, such as uploading to a server or displaying them on the page.
-    }
+input.addEventListener("change", function(){
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = this.files[0];
+  dropArea.classList.add("active");
+  showFile(); //calling function
 });
+
+
+//If user Drag File Over DropArea
+dropArea.addEventListener("dragover", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  dropArea.classList.add("active");
+  dragText.textContent = "Release to Upload File";
+});
+
+//If user leave dragged File from DropArea
+dropArea.addEventListener("dragleave", ()=>{
+  dropArea.classList.remove("active");
+  dragText.textContent = "Drag & Drop to Upload File";
+});
+
+//If user drop File on DropArea
+dropArea.addEventListener("drop", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = event.dataTransfer.files[0];
+  showFile(); //calling function
+});
+
+function showFile(){
+  let fileType = file.type; //getting selected file type
+  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
+  if(validExtensions.includes(fileType)){ //if user selected file is an image file
+    let fileReader = new FileReader(); //creating new FileReader object
+    fileReader.onload = ()=>{
+      let fileURL = fileReader.result; //passing user file source in fileURL variable
+      let imgTag = `<img src="${fileURL}" alt="">`; //creating an img tag and passing user selected file source inside src attribute
+      dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
+    }
+    fileReader.readAsDataURL(file);
+  }else{
+    alert("This is not an Image File!");
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+  }
+}
+
+
+function upload_file(e) {
+    e.preventDefault();
+    fileobj = e.dataTransfer.files[0];
+    // js_file_upload(fileobj);
+}
+
+function file_browse() {
+  document.getElementById('file').onchange = function() {
+      fileobj = document.getElementById('file').files[0];
+      // js_file_upload(fileobj);
+  };
+}
+
+ 
+
+// document.addEventListener("DOMContentLoaded", function() {
+    
+   
+//     // dropContainer.addEventListener("dragover", function(e) {
+//     //     e.preventDefault();
+//     //     dropZone.classList.add('highlight');
+//     // });
+
+//     // dropContainer.addEventListener("dragleave", function() {
+//     //     dropZone.classList.remove('highlight');
+//     // });
+
+//     // dropContainer.addEventListener("drop", function(e) {
+//     //     e.preventDefault();
+//     //     dropZone.classList.remove('highlight');
+
+//     //     const files = e.dataTransfer.files;
+//     //     handleFiles(files);
+//     // });
+
+//     // fileInput.addEventListener("change", function() {
+//     //     const files = fileInput.files;
+//     //     handleFiles(files);
+//     // });
+
+//     // function handleFiles(files) {
+//     //     const uploadedImage = document.getElementById('uploadedImage');
+//     //     const dropContainer = document.getElementById('dropContainer');
+    
+      
+//     //         // Set the src attribute of the img element to display the uploaded image
+//     //         uploadedImage.src = URL.createObjectURL(files);
+    
+//     //         // Append the image to the dropContainer
+//     //         dropContainer.appendChild(uploadedImage);
+        
+        
+
+//     //     // Handle the dropped or selected files here
+//     //     console.log(files);
+//     //     // You can add further processing for the files, such as uploading to a server or displaying them on the page.
+//     // }
+// });
 
 function openNav() {
     document.getElementById("homeSidebar").style.width = "250px";
