756b1ef2366050889f915618145cdc957b71d259
j52068mt <merve.turan@student.manchester.ac.uk>
2023-10-29 11:50:38 +0000

    last push to frontend

--- map.js
+++ map.js
@@ -47,3 +47,94 @@ function openNav() {
     document.getElementById("homeSidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
   }
+
+  //map features:
+  var map;
+        var markers = [];
+        var infowindow;
+
+        function initAutocomplete() {
+            map = new google.maps.Map(document.getElementById("googleMap"), {
+                center: { lat: -33.8688, lng: 151.2195 },
+                zoom: 13,
+                mapTypeId: "roadmap",
+            });
+
+            infowindow = new google.maps.InfoWindow();
+
+            const input = document.getElementById("pac-input");
+            const searchBox = new google.maps.places.SearchBox(input);
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+            map.addListener("bounds_changed", () => {
+                searchBox.setBounds(map.getBounds());
+            });
+
+            searchBox.addListener("places_changed", () => {
+                const places = searchBox.getPlaces();
+
+                if (places.length == 0) {
+                    return;
+                }
+
+                markers.forEach((marker) => {
+                    marker.setMap(null);
+                });
+                markers = [];
+
+                const bounds = new google.maps.LatLngBounds();
+
+                places.forEach((place) => {
+                    if (!place.geometry || !place.geometry.location) {
+                        console.log("Returned place contains no geometry");
+                        return;
+                    }
+
+                    markers.push(
+                        new google.maps.Marker({
+                            map,
+                            title: place.name,
+                            position: place.geometry.location,
+                        })
+                    );
+
+                    google.maps.event.addListener(markers[markers.length - 1], "click", function () {
+                        infowindow.setContent(place.name);
+                        infowindow.open(map, markers[markers.length - 1]);
+                    });
+
+                    if (place.geometry.viewport) {
+                        bounds.union(place.geometry.viewport);
+                    } else {
+                        bounds.extend(place.geometry.location);
+                    }
+                });
+                map.fitBounds(bounds);
+            });
+        }
+
+        function myMap() {
+            initAutocomplete();
+
+            google.maps.event.addListener(map, "click", function (event) {
+                placeMarker(event.latLng);
+            });
+
+            
+            function placeMarker(location) {
+                var marker = new google.maps.Marker({
+                    position: location,
+                    map: map,
+                });
+                markers.push(marker);
+                
+
+                var infowindow = new google.maps.InfoWindow({
+                  content: "daisy" +"<br>img"
+                });
+                    infowindow.open(map, marker);
+                
+            }
+        }
+
+        
